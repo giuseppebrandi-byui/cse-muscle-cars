238d593a9d79a1b399ec5934cb43f3c3618e1a08
giuseppebrandi-byui <bra22041@byui.edu>
2024-08-07 22:30:11 +0100

    Added grid for cars page

--- utilities/index.js
+++ utilities/index.js
@@ -37,28 +37,48 @@ Util.getNav = async function (req, res, next) {
 * ************************************ */
 Util.buildMakeGrid = async function(data){
   let grid;
-  if(data.length > 0){
-    grid = '<ul id="inv-display">'
+  if (data.length > 0) {
+    grid = '<ul class="grid grid--1-cols grid--2-cols grid--3-cols">';
     data.forEach(vehicle => { 
-      grid += '<li>'
-      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+      grid += '<li class="scrollbar-item">';
+      grid += '<div class="new-cars-card">';
+      grid += '<figure class="card-banner img-holder" style="--width: 650; --height: 433;">';
+      grid += '<img src="' + vehicle.inv_thumbnail + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model + ' on CSE Muscle Cars" width="650" height="433" loading="lazy" class="img-cover">';
+      grid += '</figure>';
+      grid += '<div class="card-content">';
+      grid += '<p class="card-subtitle">' + vehicle.inv_make + '</p>';
+      grid += '<h3 class="h3 card-title">' + vehicle.inv_model + '</h3>';
+      grid += '<div class="info-vehicle">';
+      grid += '<p>Price: $' + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</p>';
+      grid += '<p>Year: ' + vehicle.inv_year + '</p>';
+      grid += '</div>';
+      grid += '<a href="../../inv/detail/'+ vehicle.inv_id 
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Muscle Cars" /></a>'
-      grid += '<div class="namePrice">'
-      grid += '<hr />'
-      grid += '<h2>'
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
-      grid += '</h2>'
-      grid += '<span>$' 
-      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
-      grid += '</div>'
-      grid += '</li>'
+      + 'details" class="card-btn">';
+      grid += '<span class="material-symbols-rounded">arrow_forward</span>';
+      grid += ' </a>';
+      grid += '</div>';
+      grid += '</div>';
+      grid += '</li>';
+
+      // grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+      // + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+      // + 'details"><img src="' + vehicle.inv_thumbnail 
+      // +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      // +' on CSE Muscle Cars" /></a>'
+      // grid += '<div class="namePrice">'
+      // grid += '<hr />'
+      // grid += '<h2>'
+      // grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+      // + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+      // + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+      // grid += '</h2>'
+      // grid += '<span>$' 
+      // + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      // grid += '</div>'
+      // grid += '</li>'
     })
-    grid += '</ul>'
+    grid += '</ul>';
   } else { 
     grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
   }
