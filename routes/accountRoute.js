05e238bcd697a9156e518d3172c65980a3db15ca
giuseppebrandi-byui <bra22041@byui.edu>
2024-08-15 10:43:08 +0100

    Build login and register functionality

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,4 +6,7 @@ const utilities = require("../utilities");
 // Deliver login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
+// Deliver register view
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
 module.exports = router;
--- server.js
+++ server.js
@@ -56,12 +56,13 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome));
 // Inventory routes
 app.use("/inv", utilities.handleErrors(inventoryRoute));
+// Account routes
 app.use("/account", utilities.handleErrors(accountRoute));
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
 });
-// Account routes
+
 
 
 /* ***********************
