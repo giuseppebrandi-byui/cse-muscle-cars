2a104f4acdd99c8bec317cd62506e2c50cc87164
giuseppebrandi-byui <bra22041@byui.edu>
2024-08-16 09:56:55 +0100

    Add router post for process registration

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -9,4 +9,7 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Deliver register view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
+// Process registration
+router.post("/register", utilities.handleErrors(accountController.registerAccount));
+
 module.exports = router;
