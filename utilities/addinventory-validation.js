7fd45551893a82cbb0364e4f4581dcfb61821f84
giuseppebrandi-byui <bra22041@byui.edu>
2024-08-31 21:32:23 +0100

    Add inventory validation

---
+++ utilities/addinventory-validation.js
@@ -0,0 +1,146 @@
+const utilities = require(".");
+const { body, validationResult } = require("express-validator");
+const validate = {}
+
+/*  **********************************
+ *  Add inventory rules
+ * ********************************* */
+validate.addInventoryRules = () => {
+  return [
+    // make name is required.
+    body("inv_make")
+      .trim()
+      .notEmpty()
+      .escape()
+      .isLength({ min: 3 })
+      .withMessage("A valid make name is required")
+      .custom(async (inv_make) => {
+        if (!inv_make.match(/^[a-zA-Z]+$/)) {
+          throw new Error("Only alphabethical characters allowed.");
+        }
+      }),
+    
+    // model name is required.
+    body("inv_model")
+      .trim()
+      .notEmpty()
+      .escape()
+      .isLength({ min: 3 })
+      .withMessage("A valid model name is required"),
+    
+    // a description is required.
+    body("inv_description")
+      .trim()
+      .notEmpty()
+      .escape()
+      .isLength({ min: 10 })
+      .withMessage("A description of the vehicle is required"),
+    
+    // an image path is required.
+    body("inv_image")
+      .trim()
+      .notEmpty()
+      .escape()
+      .withMessage("Please provide a link to the vehicle image"),
+    
+    // an image thumbnail path is required.
+    body("inv_thumbnail")
+      .trim()
+      .notEmpty()
+      .escape()
+      .withMessage("Please provide a link to the vehicle image thumbnail"),
+    
+    // vehicle's price is required.
+    body("inv_price")
+      .trim()
+      .notEmpty()
+      .escape()
+      .isLength({ min: 2 })
+      .withMessage("Please provide a valid price")
+      .custom(async (inv_price) => {
+         if (!inv_price.match(/^\d+$/)) {
+          throw new Error("Only digits allowed.");
+        }
+      }),
+    
+    // vehicle's year is required.
+    body("inv_year")
+      .trim()
+      .notEmpty()
+      .escape()
+      .isLength({ min: 4, max:4 })
+      .withMessage("Please provide 4 digits year.")
+      .custom(async (inv_year) => {
+         if (!inv_year.match(/^\d+$/)) {
+          throw new Error("Please provide 4 digits year.");
+        }
+      }),
+    
+    // vehicle's mileage is required.
+    body("inv_miles")
+      .trim()
+      .notEmpty()
+      .escape()
+      .withMessage("Please provide a valid vehicle's mileage.")
+      .custom(async (inv_miles) => {
+         if (!inv_miles.match(/^\d+$/)) {
+          throw new Error("Please provide a numeric value.");
+        }
+      }),
+    
+     // color is required.
+    body("inv_color")
+      .trim()
+      .notEmpty()
+      .escape()
+      .withMessage("A valid color is required")
+      .custom(async (inv_color) => {
+        if (!inv_color.match(/^[a-zA-Z]+$/)) {
+          throw new Error("Only alphabethical characters allowed.");
+        }
+      }),
+  ]
+};
+
+
+
+/*  **********************************
+ *  Check add new vehicle data
+ * ********************************* */
+validate.checkNewInventoryData = async (req, res, next) => { 
+  const {
+    inv_make,
+    inv_model,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_year,
+    inv_miles,
+    inv_color
+  } = req.body;
+  let errors = [];
+  errors = validationResult(req);
+  if (!errors.isEmpty()) { 
+    let nav = await utilities.getNav();
+    res.render("inventory/add-inventory", {
+      errors,
+      title: "Add New Vehicle",
+      nav,
+      inv_make,
+      inv_model,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_year,
+      inv_miles,
+      inv_color,
+    });
+    return;
+  }
+  next();
+}
+
+module.exports = validate;
+
