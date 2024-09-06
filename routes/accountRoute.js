630513fbb23fbfcef02b355714e63f926dc54af2
giuseppebrandi-byui <bra22041@byui.edu>
2024-09-06 10:28:25 +0100

    Added authentication functionality

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -21,17 +21,15 @@ router.post(
 // Process the login attempt
 router.post(
   "/login",
-  (req, res) => {
-    res.status(200).send("login process");
-  }
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+  utilities.handleErrors(accountController.accountLogin)
+// utilities.handleErrors(accountModel.checkExistingEmail);
 );
 
-// TO FIX
-// router.post(
-//   "/login",
-//   regValidate.loginRules(),
-//   regValidate.checkLoginData,
-//   utilities.handleErrors(accountModel.checkExistingEmail)
-// );
+// Deliver account management view
+router.get("/",
+  regValidate.checkLoginData,
+  utilities.handleErrors(accountController.buildAccountManagement));
 
 module.exports = router;
